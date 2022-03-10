65e214c00619550962e37bb21c44d6bc3e2f93bd
Yash Jasani <jasaniyash13@gmail.com>
2022-03-10 09:09:23 +0530

    Time: 4 ms (87.91%), Space: 10.7 MB (30.84%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -3,28 +3,37 @@ public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         
         map<int, int> num1;
-        map<int, int> num2;
+        // map<int, int> num2;
         vector<int> num;
         
+        // for(auto it : nums1){
+        //     if(!num1.empty() && num1.find(it) != num1.end()){
+        //         num1.at(it) += 1;
+        //     }else{
+        //         num1.insert({it, 1});
+        //     }   
+        // }
+        // for(auto it : nums2){
+        //     if(!num2.empty() && num2.find(it) != num2.end()){
+        //         num2.at(it) += 1;
+        //     }else{
+        //         num2.insert({it, 1});
+        //     }   
+        // }
+        // for(auto it = num1.begin(); it != num1.end(); it++){
+        //     if(num2.find(it->first) != num2.end()){
+        //         for(int i = 0; i < min(it->second, (num2.find(it->first))->second); i++){
+        //             num.push_back(it->first);
+        //         }
+        //     }
+        // }
+        
         for(auto it : nums1){
-            if(!num1.empty() && num1.find(it) != num1.end()){
-                num1.at(it) += 1;
-            }else{
-                num1.insert({it, 1});
-            }   
+            num1[it]++;
         }
         for(auto it : nums2){
-            if(!num2.empty() && num2.find(it) != num2.end()){
-                num2.at(it) += 1;
-            }else{
-                num2.insert({it, 1});
-            }   
-        }
-        for(auto it = num1.begin(); it != num1.end(); it++){
-            if(num2.find(it->first) != num2.end()){
-                for(int i = 0; i < min(it->second, (num2.find(it->first))->second); i++){
-                    num.push_back(it->first);
-                }
+            if(num1[it]-- > 0){
+                num.push_back(it);
             }
         }
         return num;
