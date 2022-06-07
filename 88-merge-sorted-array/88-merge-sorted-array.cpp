0f0ddfc005d129d6f670a2d0afc9ac4da12840f0
Yash Jasani <jasaniyash13@gmail.com>
2022-06-07 10:35:24 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (30.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        multiset<int> s;
-        int i;
-        for(i=0;i<m;i++){
-            s.insert(nums1[i]);
+        int i=m-1, j=n-1, k=nums1.size()-1;
+        while(i>=0 && j>=0){
+            if(nums1[i] <= nums2[j]){
+                nums1[k--] = nums2[j--];
+            }
+            else{
+                nums1[k--] = nums1[i--];
+            }
         }
-        for(i=0;i<n;i++){
-            s.insert(nums2[i]);
-        }
-        i=0;
-        for(auto it=s.begin();it!=s.end();it++){
-            nums1[i++] = *it;
-        }
-        
+        while(j>=0) nums1[k--] = nums2[j--];
     }
 };
