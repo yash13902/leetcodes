8b63fc861933d02bdd553220feafc87e5f43edd1
Yash Jasani <jasaniyash13@gmail.com>
2022-07-06 11:53:55 +0530

    Time: 3 ms (60.76%), Space: 6.1 MB (18.24%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     
     int fib(int n) {
-        if(n == 1) return 1;
-        if(n == 0) return 0;
-        return fib(n-1) + fib(n-2);
+        return (int)((pow(((sqrt(5) + 1)/2),n) - pow((-(sqrt(5) + 1)/2),-n))/sqrt(5));
     }
 };
