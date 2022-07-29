485c616daf51fd5b536e764ed9a0a9db4535d4c3
Yash Jasani <jasaniyash13@gmail.com>
2022-07-29 21:12:36 +0530

    Time: 7 ms (8.94%), Space: 5.9 MB (43.20%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        if(n==0) return 1;
+        if(x==0) return 0;
+        if(x==1) return 1;
+        double mul=1;
+        long copy = abs(n);
+        cout << copy << endl;
+        while(copy > 1){
+            if(copy%2==0){
+                x = x*x;
+                copy /= 2;
+                cout << x << " " << copy << " " << mul << endl;
+            }else{
+                copy--;
+                mul *= x;
+                x = x*x;
+                copy /= 2;
+                cout << x << " " << copy << " " << mul << endl;
+            }
+        }
+        if(n > 0){
+            return mul*x;
+        }
+        return 1/(mul*x);
     }
 };
