f58537b7d4edf33bd8bea51d7da37c106b4ecadf
Yash Jasani <jasaniyash13@gmail.com>
2022-07-29 21:41:07 +0530

    Time: 31 ms (48.03%), Space: 19.5 MB (92.00%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,14 +1,40 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int> m;
-        for(auto c : nums){
-            m[c]++;
+        
+        sort(nums.begin(),nums.end());
+        int count=1,m=0,ans;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i] == nums[i-1]){
+                count++;
+            }else{
+                if(m<count){
+                    ans = nums[i-1];
+                    m = count;
+                }
+                count=1;
+            }
         }
-        map<int,int>::iterator x = max_element(m.begin(), m.end(), [](pair<int,int> a, pair<int,int> b){
-            return b.second > a.second;
-        });
-        cout << x->second << endl;
-        return x->first;
+        if(m<count){
+            ans = nums[nums.size()-1];
+            m = count;
+        }
+        return ans;
+        
+        
+        
+        
+        
+        
+        
+        // map<int, int> m;
+        // for(auto c : nums){
+        //     m[c]++;
+        // }
+        // map<int,int>::iterator x = max_element(m.begin(), m.end(), [](pair<int,int> a, pair<int,int> b){
+        //     return b.second > a.second;
+        // });
+        // cout << x->second << endl;
+        // return x->first;
     }
 };
