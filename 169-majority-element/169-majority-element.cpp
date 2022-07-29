b7dd34152b0d288b58c6a8d8fe32b24a1e6a246b
Yash Jasani <jasaniyash13@gmail.com>
2022-07-29 21:50:57 +0530

    Time: 22 ms (82.68%), Space: 19.7 MB (12.88%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,30 +2,36 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
-        sort(nums.begin(),nums.end());
-        int count=1,m=0,ans;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i] == nums[i-1]){
-                count++;
-            }else{
-                if(m<count){
-                    ans = nums[i-1];
-                    m = count;
-                }
-                count=1;
+        int count=0,candidate=0;
+        
+        for(auto c : nums){
+            if(count == 0){
+                candidate = c;
             }
+            if(c == candidate) count++;
+            else count--;
         }
-        if(m<count){
-            ans = nums[nums.size()-1];
-            m = count;
-        }
-        return ans;
         
+        return candidate;
         
-        
-        
-        
-        
+        // sort(nums.begin(),nums.end());
+        // int count=1,m=0,ans;
+        // for(int i=1;i<nums.size();i++){
+        //     if(nums[i] == nums[i-1]){
+        //         count++;
+        //     }else{
+        //         if(m<count){
+        //             ans = nums[i-1];
+        //             m = count;
+        //         }
+        //         count=1;
+        //     }
+        // }
+        // if(m<count){
+        //     ans = nums[nums.size()-1];
+        //     m = count;
+        // }
+        // return ans;
         
         // map<int, int> m;
         // for(auto c : nums){
