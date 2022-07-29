f5c1bb5ecfaba7c80d4b31e0c69e9990f843433b
Yash Jasani <jasaniyash13@gmail.com>
2022-07-29 19:35:53 +0530

    Time: 16 ms (6.81%), Space: 9.6 MB (5.10%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -2,54 +2,94 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target)
 {
-    if (matrix.size() == 0 || matrix[0].size() == 0)
-    {
-        return false;
-    }
-
-    int low;
-    int high;
-
-    // first search in first column, use binary search
-    for (low = 0, high = matrix.size() - 1; low <= high;)
-    {
-        int middle = (low + high) / 2;
-        if (matrix[middle][0] < target)
-        {
-            low = middle + 1;
-        }
-        else if (matrix[middle][0] > target)
-        {
-            high = middle - 1;
-        }
-        else
-        {
-            return true;
-        }
-    }
-
-    // when above loop ends, search in row[high]
-    int row = high;
-    if (row >= 0)
-    {
-        for (low = 0, high = matrix[row].size() - 1; low <= high;)
-        {
-            int middle = (low + high) / 2;
-            if (matrix[row][middle] < target)
-            {
-                low = middle + 1;
-            }
-            else if (matrix[row][middle] > target)
-            {
-                high = middle - 1;
-            }
-            else
-            {
+        
+        int i=0,j=matrix[0].size()-1;
+        while(j >= 0 && i < matrix.size()){
+            if(matrix[i][j] == target){
                 return true;
+            }else if(j==0 && i==matrix.size()-1){
+                return false;
+            }else if(target > matrix[i][j]){
+                i++;
+            }else if(target < matrix[i][j]){
+                j--;
             }
         }
-    }
+        return false;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//     if (matrix.size() == 0 || matrix[0].size() == 0)
+//     {
+//         return false;
+//     }
 
-    return false;
+//     int low;
+//     int high;
+
+//     // first search in first column, use binary search
+//     for (low = 0, high = matrix.size() - 1; low <= high;)
+//     {
+//         int middle = (low + high) / 2;
+//         if (matrix[middle][0] < target)
+//         {
+//             low = middle + 1;
+//         }
+//         else if (matrix[middle][0] > target)
+//         {
+//             high = middle - 1;
+//         }
+//         else
+//         {
+//             return true;
+//         }
+//     }
+
+//     // when above loop ends, search in row[high]
+//     int row = high;
+//     if (row >= 0)
+//     {
+//         for (low = 0, high = matrix[row].size() - 1; low <= high;)
+//         {
+//             int middle = (low + high) / 2;
+//             if (matrix[row][middle] < target)
+//             {
+//                 low = middle + 1;
+//             }
+//             else if (matrix[row][middle] > target)
+//             {
+//                 high = middle - 1;
+//             }
+//             else
+//             {
+//                 return true;
+//             }
+//         }
+//     }
+
+//     return false;
+        
 }
 };
