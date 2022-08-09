16fc64bfbb345729bafd8bc38e5db2b1f5585ef0
Yash Jasani <jasaniyash13@gmail.com>
2022-08-09 12:45:24 +0530

    Time: 1827 ms (5.00%), Space: 244.6 MB (5.00%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -7,16 +7,17 @@ public:
             return result;
         
         sort(nums.begin(), nums.end());
-        int n=nums.size()-1;
+        int n = nums.size()-1;
         int i,j,low,high;
         long new_target;
         for(i=0;i<n-1;i++){
             for(j=i+1;j<n;j++){
-                vector<int> v(4,0);
-                low = j+1; 
+                low=j+1;
                 high = n;
-                while(low < high){
-                    new_target = target - nums[i];
+                while(low<high){
+                    vector<int> v(4,0);
+                    new_target = target;
+                    new_target -= nums[i];
                     new_target -= nums[j];
                     new_target -= nums[low];
                     new_target -= nums[high];
@@ -26,18 +27,16 @@ public:
                         v[2] = nums[high];
                         v[3] = nums[j];
                         result.push_back(v);
-                        while(low < high && nums[low] == nums[low+1]) low++;
-                        while(low < high && nums[high] == nums[high-1]) high--;
+                        while(low<high && nums[low] == nums[low+1]) low++;
+                        while(low<high && nums[high] == nums[high-1]) high--;
                         low++;
                         high--;
-                    }else if (0 > new_target) {
-                        high--;
-                    } else if(0 < new_target){
-                        low++;
                     }
+                    else if(new_target < 0) high--;
+                    else if(new_target > 0) low++;
                 }
-                while(i < n-1 && nums[i] == nums[i+1]) i++;
-                while(j < n && nums[j] == nums[j+1]) j++;
+                while(i<n-1 && nums[i] == nums[i+1]) i++;
+                while(j<n && nums[j] == nums[j+1]) j++;
             }
         }
         return result;
