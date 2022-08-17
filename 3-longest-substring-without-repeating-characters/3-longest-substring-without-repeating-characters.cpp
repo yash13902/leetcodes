4a178900c3ef87e7f9c247cdf12e98c540adf930
Yash Jasani <jasaniyash13@gmail.com>
2022-08-17 21:39:31 +0530

    Time: 562 ms (8.74%), Space: 13.5 MB (11.64%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,15 +1,40 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> d(256, -1);
-        int maxLen=0,start=-1;
-        for(int i=0;i<s.size();i++){
-            if(d[s[i]] > start){
-                start = d[s[i]];
+        // vector<int> d(256, -1);
+        // int maxLen=0,start=-1;
+        // for(int i=0;i<s.size();i++){
+        //     if(d[s[i]] > start){
+        //         start = d[s[i]];
+        //     }
+        //     d[s[i]] = i;
+        //     maxLen = max(maxLen, i-start);
+        // }
+        // return maxLen;
+        set<int> c;
+        int l=0,r=0,i,le=0,m;
+        while(l<s.size() && r<s.size()){
+            if(c.size()==0 || c.find(s[r]) == c.end()){
+                c.insert(s[r++]);
+                m=r-l;
+                for(char d:c){
+                    cout << "not found " << d << " ";
+                }
+                cout << endl;
+                le = max(le, m);
+            }else{
+                cout << "found " << s[r] << endl;
+                while(c.find(s[r]) != c.end()){
+                    cout << "removing " << s[l] << endl;
+                    c.erase(s[l]);
+                    l++;
+                }
+                cout << "now inserting " << s[r] << endl;
+                c.insert(s[r++]);
+                m=r-l;
+                le = max(le, m);
             }
-            d[s[i]] = i;
-            maxLen = max(maxLen, i-start);
         }
-        return maxLen;
+        return le;
     }
 };
