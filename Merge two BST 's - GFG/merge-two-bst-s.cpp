6af3df5c6a3b84f699d9d9c457a7f6fd1bfa44b3
Yash Jasani <jasaniyash13@gmail.com>
2023-04-05 23:48:05 +0530

    Added solution - LeetHub

---
+++ Merge two BST 's - GFG/merge-two-bst-s.cpp
@@ -0,0 +1,200 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+
+
+// } Driver Code Ends
+/*
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data) {
+        data = data;
+        left = right = NULL;
+    }
+};
+*/
+#include<bits/stdc++.h> 
+class Solution
+{
+    public:
+    //Function to return a list of integers denoting the node 
+    //dataues of both the BST in a sorted order.
+    vector<int> merge(Node *root1, Node *root2)
+    {
+       //Your code here
+       
+       vector<int> ans;
+       stack<Node*> s1,s2;
+       while(root1){
+           s1.push(root1);
+           root1 = root1->left;
+       }
+       while(root2){
+           s2.push(root2);
+           root2 = root2->left;
+       }
+       Node* temp;
+       Node* temp1;
+       while(!s1.empty() || !s2.empty()){
+           if(s1.empty()){
+               while(!s2.empty()){
+                   temp = s2.top();
+                   s2.pop();
+                   ans.push_back(temp->data);
+                   temp = temp->right;
+                   while(temp){
+                       s2.push(temp);
+                       temp = temp->left;
+                   }
+               }
+           }
+           else if(s2.empty()){
+               while(!s1.empty()){
+                   temp = s1.top();
+                   s1.pop();
+                   ans.push_back(temp->data);
+                   temp = temp->right;
+                   while(temp){
+                       s1.push(temp);
+                       temp = temp->left;
+                   }
+               }
+           }
+           else{
+               temp = s1.top();
+               temp1 = s2.top();
+               if(temp->data <= temp1->data){
+                   ans.push_back(temp->data);
+                   s1.pop();
+                   temp = temp->right;
+                   while(temp){
+                       s1.push(temp);
+                       temp = temp->left;
+                   }
+               }else{
+                   ans.push_back(temp1->data);
+                   s2.pop();
+                   temp1 = temp1->right;
+                   while(temp1){
+                       s2.push(temp1);
+                       temp1 = temp1->left;
+                   }
+               }
+           }
+       }
+       return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+        string s; 
+       getline(cin, s);
+       Node* root1 = buildTree(s);
+
+       getline(cin, s);
+       Node* root2 = buildTree(s);
+    
+       // getline(cin, s);
+       Solution obj;
+       vector<int> vec = obj.merge(root1, root2);
+       for(int i=0;i<vec.size();i++)
+            cout << vec[i] << " ";
+        cout << endl;
+       ///cout<<"~"<<endl;
+   }
+   return 0;
+}
+// } Driver Code Ends
