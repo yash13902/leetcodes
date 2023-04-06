21d56ef72530f68df886664ce329466a3b246fa1
Yash Jasani <jasaniyash13@gmail.com>
2023-04-06 23:11:20 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -6,28 +6,52 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    bool isCycle(int v, vector<int> adj[]) {
-        // Code here
-        
-        queue<pair<int,int>> q;
-        int vis[v] = {0};
-        for(int i=0;i<v;i++){
-            if(vis[i] == 0){
-                q.push({i,-1});
-                vis[i] = 1;
-                while(!q.empty()){
-                    pair<int,int> node = q.front();
-                    q.pop();
-                    for(auto it : adj[node.first]){
-                        if(it == node.second) continue;
-                        if(vis[it] == 1) return true;
-                        vis[it] = 1;
-                        q.push({it,node.first});
-                    }
-                }   
+    
+    bool flag = false;
+    
+    void cy(int i, vector<int> adj[], int parent, int vis[]){
+        if(flag) return;
+        vis[i] = 1;
+        for(auto it : adj[i]){
+            if(it == parent) continue;
+            if(vis[it] == 1){
+                flag = true;
+            }else{
+                cy(it, adj, i, vis);
             }
         }
-        return false;
+        
+    }
+    
+    bool isCycle(int v, vector<int> adj[]) {
+        // Code here
+        //bfs
+        // queue<pair<int,int>> q;
+        int vis[v] = {0};
+        // for(int i=0;i<v;i++){
+        //     if(vis[i] == 0){
+        //         q.push({i,-1});
+        //         vis[i] = 1;
+        //         while(!q.empty()){
+        //             pair<int,int> node = q.front();
+        //             q.pop();
+        //             for(auto it : adj[node.first]){
+        //                 if(it == node.second) continue;
+        //                 if(vis[it] == 1) return true;
+        //                 vis[it] = 1;
+        //                 q.push({it,node.first});
+        //             }
+        //         }   
+        //     }
+        // }
+        // return false;
+        
+        for(int i=0;i<v;i++){
+            if(vis[i] == 0){
+                cy(i, adj, -1, vis);
+            }
+        }
+        return flag;
     }
 };
 
