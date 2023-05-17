ebe3922dbcabbeaa52cfb84311253a11796ced6e
Yash Jasani <jasaniyash13@gmail.com>
2023-05-17 11:27:42 +0530

    Added solution - LeetHub

---
+++ Disjoint set (Union-Find) - GFG/disjoint-set-union-find.cpp
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+int find(int a[],int x);
+void unionSet(int a[],int x ,int z);
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int a[n+1];
+		for(int i=1;i<=n;i++)
+			a[i]=i;
+		int k;
+		cin>>k;
+		for(int i=0;i<k;i++){
+			string s;
+			cin>>s;
+			if(s=="UNION"){
+				int z,x;
+				cin>>x>>z;
+				unionSet(a,x,z);
+			}
+			else{
+				int x;
+				cin>>x;
+				int parent=find(a,x);
+				cout<<parent<<" ";
+			}
+		}
+		cout<<endl;
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the functions below*/
+
+class DisjointSet{
+    vector<int> parent,rank,size;
+    public : 
+    DisjointSet(int n){
+        parent.resize(n+1);
+        rank.resize(n+1, 0);
+        size.resize(n+1, 1);
+        for(int i=0;i<=n;i++){
+            parent[i] = i;
+        }
+    }
+    
+    int findUPar(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    
+    void unionByRank(int u, int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+        if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            
+        }else if(rank[ulp_u] > rank[ulp_v]){
+            parent[ulp_v] = ulp_u;
+        }else{
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    
+    void unionBySize(int u, int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+
+int find(int parent[],int node)
+{
+       //add code here
+       if(parent[parent[node]] == parent[node]) return parent[node];
+       return find(parent, parent[node]);
+
+}
+void unionSet(int parent[],int u,int v)
+{
+	//add code here.
+	parent[find(parent, u)] = find(parent, v);
+	
+}
