92212bf299d71d5dec790ab2cee1e67b7bc8a7af
Yash Jasani <jasaniyash13@gmail.com>
2023-05-22 18:05:53 +0530

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.cpp
+++ Geek's Training - GFG/geeks-training.cpp
@@ -27,8 +27,26 @@ class Solution {
   
     int maximumPoints(vector<vector<int>>& points, int n) {
         // Code here
-        vector<vector<int>> dp(n+1, vector<int>(5,-1));
-        return recur(points, n-1, 3, dp);
+        // vector<vector<int>> dp(n+1, vector<int>(5,-1));
+        // return recur(points, n-1, 3, dp);
+        
+        vector<vector<int>> dp(n, vector<int>(4,0));
+        dp[0][0] = max(points[0][1], points[0][2]);
+        dp[0][1] = max(points[0][0], points[0][2]);
+        dp[0][2] = max(points[0][0], points[0][1]);
+        dp[0][3] = max(points[0][0], max(points[0][1], points[0][2]));
+        for(int day=1;day<n;day++){
+            for(int last=0;last<4;last++){
+                int maxi=0;
+                for(int task=0;task<3;task++){
+                    if(task==last) continue;
+                    int point = points[day][task] + dp[day-1][task];
+                    maxi = max(maxi, point);
+                }
+                dp[day][last] = maxi;
+            }
+        }
+        return dp[n-1][3];
     }
 };
 
