ac6cc6daa8f35b1d792c17906d6fe20e001379e0
Yash Jasani <jasaniyash13@gmail.com>
2023-07-29 10:42:37 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -6,34 +6,24 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    
-    int element(int arr1[], int arr2[], int n, int m, int k){
-        if(n > m) return element(arr2, arr1, m, n, k);
-        
-        int low=max(0,k-m),high=min(k,n);
-        int cut1,cut2,l1,l2,r1,r2;
-        while(low<=high){
-            cut1 = low + (high-low)/2;
-            cut2 = k - cut1;
-            
-            l1 = (cut1==0)?INT_MIN:arr1[cut1-1];
-            l2 = (cut2==0)?INT_MIN:arr2[cut2-1];
-            r1 = (cut1==n)?INT_MAX:arr1[cut1];
-            r2 = (cut2==m)?INT_MAX:arr2[cut2];
-            
-            if(l1 <= r2 && l2 <= r1){
-                return max(l1,l2);
-            }else if(l1 > r2) high = cut1-1;
-            else low = cut1+1;
-        }
-        return 0;
-    }
-    
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {
-        if(k == 1) return min(arr1[0], arr2[0]);
-        if(k == n+m) return max(arr1[n-1], arr2[m-1]);
-        return element(arr1, arr2, n, m, k);
+        priority_queue<int> pq;
+        for(int i=0;i<n;i++){
+            if(pq.empty() || pq.size() < k) pq.push(arr1[i]);
+            else if(pq.size() == k){
+                pq.push(arr1[i]);
+                pq.pop();
+            }
+        }
+        for(int i=0;i<m;i++){
+            if(pq.empty() || pq.size() < k) pq.push(arr2[i]);
+            else if(pq.size() == k){
+                pq.push(arr2[i]);
+                pq.pop();
+            }
+        }
+        return pq.top();
     }
 };
 
